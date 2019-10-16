78eae8f14c778d6fd3cf413a0617e6be2fb01891
Vitaly <vitalykuklin@gmail.com>
2019-10-17 06:10:55 +0700

    Feature: 2-module 2-task solution [ref:splitString stream]

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,12 +4,23 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.textData = '';
   }
 
   _transform(chunk, encoding, callback) {
+    const incomingData = chunk.toString();
+    const currentData = this.textData + incomingData;
+    const filteredData = currentData.split(EOL);
+    const lastChild = filteredData.pop();
+    filteredData.map((filteredChunk) => {
+      this.push(filteredChunk);
+    });
+    this.textData = lastChild;
+    callback();
   }
 
   _flush(callback) {
+    callback(null, this.textData);
   }
 }
 
