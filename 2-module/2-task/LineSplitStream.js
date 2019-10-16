814605d3e3dc339b4f6aa1963893913f166f5ed6
Vitaly <vitalykuklin@gmail.com>
2019-10-17 06:26:04 +0700

    Feature: ref 2-module 2 -task

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -1,26 +1,39 @@
 const stream = require('stream');
-const os = require('os');
+const {EOL} = require('os');
 
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.encoding = options.encoding;
     this.textData = '';
   }
 
   _transform(chunk, encoding, callback) {
-    const incomingData = chunk.toString();
+    const incomingData = this.setEncoding(chunk);
     const currentData = this.textData + incomingData;
+    if (currentData.includes(EOL)) {
+      this.splitLogic(currentData);
+    } else {
+      this.textData += incomingData;
+    }
+    callback();
+  }
+
+  _flush(callback) {
+    callback(null, this.textData);
+  }
+
+  setEncoding(chunk) {
+    return this.encoding ? chunk.toString(this.encoding) : chunk.toString();
+  }
+
+  splitLogic(currentData, newLineSymbol) {
     const filteredData = currentData.split(EOL);
     const lastChild = filteredData.pop();
     filteredData.map((filteredChunk) => {
       this.push(filteredChunk);
     });
     this.textData = lastChild;
-    callback();
-  }
-
-  _flush(callback) {
-    callback(null, this.textData);
   }
 }
 
